8a4f2d2537ac652ef73426d1dda76188aa252482
siluna <2261906492@qq.com>
2019-01-17 09:20:18 +0800

    显示敏感字符

--- main.c
+++ main.c
@@ -50,6 +50,19 @@
                 scanf("%c",&x);
                 scanf("%c",&x);
             }
+            if (code == 3)
+        {
+            printf("xiansgusuoyouyijingtianjiademinganzifu\n");
+
+            for (int i = 0; i < index; i++)
+            {
+                printf("%c\n", arr[i]);
+            }
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
         }
         
      return 0;
