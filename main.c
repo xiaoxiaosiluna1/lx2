a1a616c51ed7e656bed726496a8e00fdf766baaf
siluna <2261906492@qq.com>
2019-01-17 09:14:59 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>  
-    
+    char arr[1024];
+    int index = 0;
+    int isMingan(char x){
+    int count=0;
+    for(int i=0;i<index;i++){
+        if(x==arr[i]){
+            count++;
+        }
+    }
+    if(count==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
     int main()
     {
         printf("jinruminganzifuguanlixitong");
+        while(1)
+        {
+            printf("1---tianjiminganzifu\n");
+            printf("2---shanchuzuihouyigeminganzifu\n");
+            printf("3---xianshisuoyouminganzifu\n");
+            printf("4---tihuansuoyouminganzifu\n");
+            printf("5---tuichu\n");
+            printf("qingshuruxiangduiyingdegongneng:\n");
+            int code;
+            scanf("%d",&code);
+            if(code ==1)
+            {
+                printf("tianjiayigeminganzifu\n");
+                char m;
+                scanf("%c",&m);
+                scanf("%c",&m);
+                arr[index]=m;
+                index++;
+                printf("tianjiachenggong,dianjihuichejixu\n");
+                char x;
+                scanf("%c",&x);
+                scanf("%c",&x);
+            }
+        }
         
      return 0;
 
